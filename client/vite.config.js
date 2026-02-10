be547fff9f387af46ebda767c189bbf5285bf8bd
Shamsuddeen Abdulazeez <113597563+thesardauna@users.noreply.github.com>
2026-02-10 17:18:11 +0100

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,9 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      "/api": "http://localhost:5000"
-    }
-  }
+  base: "/Lissafi/"
 });
