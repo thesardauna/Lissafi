c92b8a4f858f9f8e5399ee8d25ca601b7aa6f5a1
Shamsuddeen Abdulazeez <113597563+thesardauna@users.noreply.github.com>
2026-02-10 12:44:48 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  // IMPORTANT: Change "lissafi" to your GitHub repository name if different.
-  base: "/lissafi/"
+  base: "/Lissafi/"   // MUST match repo name exactly (case-sensitive)
 });
